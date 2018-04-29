b1686abc22a8419889a238e7bb5c4226a205b57a
Glenn Cullen <glennncullen@gmail.com>
2018-04-29 14:27:10 +0000

    commented code

--- app/mirror/weather_display.py
+++ app/mirror/weather_display.py
@@ -16,22 +16,28 @@ class WeatherFeed(Frame):
 		
 		self.configure(background='black')
 		
+		# variables for ip
 		self.ip = ''
+		# dictionary of location 
 		self.location = {}
+		# array of current forecast jsons
 		self.forecast = []
+		# currently selected day
 		self.selected_day = 0
 		
+		# image for selected day
 		self.selected_YES_img = Image.open("assets/selected_YES.jpg")
 		self.selected_YES_img = self.selected_YES_img.resize((16, 16))
 		self.selected_YES_img = self.selected_YES_img.convert('RGB')
 		self.selected_YES = ImageTk.PhotoImage(self.selected_YES_img)
 		
+		# image for not selected day
 		self.selected_NO_img = Image.open("assets/selected_NO.jpg")
 		self.selected_NO_img = self.selected_NO_img.resize((16, 16))
 		self.selected_NO_img = self.selected_NO_img.convert('RGB')
 		self.selected_NO = ImageTk.PhotoImage(self.selected_NO_img)
 		
-		# request to get ip
+		# request to get ip through api.ipify.org
 		try:
 			ip_api_url = 'https://api.ipify.org'
 			self.ip = requests.get(ip_api_url).text
@@ -39,15 +45,17 @@ class WeatherFeed(Frame):
 			print "Error requesting ip: %s" % error
 		# request to get location
 		try:
+			# using ip, get location from geoplugin and store in location dict
 			location_api_url = 'http://geoplugin.net/json.gp?ip=%s' % self.ip
 			response = requests.get(location_api_url)
 			self.location = json.loads(response.text)
 		except Exception as error:
 			print "Error requesting location: %s" % error
-		# request to get weather data
 		try:
+			# using subscribed to api key, build request url using it and the location
 			weather_api_key = "7e6cc30a4f8ea38857191b4bf0264bcf"
 			weather_api_url = 'http://api.openweathermap.org/data/2.5/forecast?q=%s,%s&mode=json&appid=%s' % ((self.location["geoplugin_countryName"].replace(" ", "_")), self.location["geoplugin_countryCode"], weather_api_key)
+			# GET weather for location
 			response = requests.get(weather_api_url)
 			response_json = json.loads(response.text)
 			weather = response_json["list"]
@@ -69,19 +77,19 @@ class WeatherFeed(Frame):
 		
 		self.build_forecast()
 	
-	# create frames for forecast
+	# destroy previous frames and create frames for forecast
 	def build_forecast(self):
 		for frame in self.forecast_frame.winfo_children():
 			frame.destroy()
-		
 		for day in self.forecast:
 			frame = Day(self.forecast_frame, day, (day is self.forecast[self.selected_day]))
 			frame.pack(side=TOP, anchor=CENTER, pady=20, fill=X)
 	
+	# when display is in focus, publish the forecast for selected day
 	def on_focus(self, *args):
 		publish.publish(args[0], "/iotappdev/weather/day/", self.forecast[self.selected_day], args[1])
 	
-	# move day in direction given
+	# when flick hat receives vertical gesture, move day in direction given
 	def change_vertical_focus(self, direction, *args):
 		self.forecast_frame.winfo_children()[self.selected_day].selected_lbl.configure(image=self.selected_NO)
 		self.forecast_frame.winfo_children()[self.selected_day].selected_lbl.image = self.selected_NO
@@ -94,17 +102,21 @@ class WeatherFeed(Frame):
 		self.forecast_frame.winfo_children()[self.selected_day].selected_lbl.image = self.selected_YES
 		publish.publish(args[0], "/iotappdev/weather/day/", self.forecast[self.selected_day], args[1])
 	
+	# do nothing on double tap
 	def double_tap(self, *args):
 		return
 	
+	# when flick hat receives airwheel gesture, move day in direction given
 	def airwheel(self, direction, *args):
 		self.change_vertical_focus(direction, args[0], args[1])
 	
+	# on update, reset selected day and rebuild forecast frames
 	def update(self):
 		self.selected_day = 0
 		self.build_forecast()
 		return
 	
+	# create forecast entry taking only relevant data
 	def create_entry(self, entry):
 		new_entry = {}
 		new_entry["location"] = self.location["geoplugin_countryName"]
@@ -158,6 +170,8 @@ class WeatherFeed(Frame):
 			return 'N'
 
 # small frame for displaying day's weather
+# if day is selected, selected_YES image is 
+# put beside day's forecast
 class Day(Frame):
 	def __init__(self, parent, forecast, selected):
 		Frame.__init__(self, parent, bg='black')
